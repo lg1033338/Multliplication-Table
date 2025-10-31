ee648c21b6e56bf638f39faa39b49baee2923926
lg1033338 <lg1033338@otc.edu>
2025-10-30 19:21:42 -0500

    Multiplication Table
    A multiplication table

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿Console.WriteLine("Welcome to my multiplication table!");
+int count;
+
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 2}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 3}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 4}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 5}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 6}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 7}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 8}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 9}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 10}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++)
+{
+    Console.Write($" | {count * 11}");
+
+}
+
+Console.WriteLine("");
+for (count = 1; count <= 12; count++) 
+{
+    Console.Write($" | {count * 12}");
+
+}
